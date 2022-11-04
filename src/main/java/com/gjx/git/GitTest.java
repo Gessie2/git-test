c803e783cb356d36eff9e88aec1e1585eb4898e5
gjx <gjx@1502660122.com>
2022-11-04 17:47:32 +0800

    second commit

--- src/main/java/com/gjx/git/GitTest.java
+++ src/main/java/com/gjx/git/GitTest.java
@@ -3,6 +3,7 @@ package com.gjx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git222");
 
     }
 }
