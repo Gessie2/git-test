f39dc96057deac5944dd25b4e0031dd20dfc907f
gjx <gjx@1502660122.com>
2022-11-06 13:49:17 +0800

    hot-fix commit1

--- src/main/java/com/gjx/git/GitTest.java
+++ src/main/java/com/gjx/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git222");
-
+        System.out.println("hello 44");
     }
 }
