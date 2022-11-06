eecf8a7bc152b841f50cccf64c1ab694751f36f1
gjx <gjx@1502660122.com>
2022-11-06 14:12:16 +0800

    push commit

--- src/main/java/com/gjx/git/GitTest.java
+++ src/main/java/com/gjx/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello 44");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
